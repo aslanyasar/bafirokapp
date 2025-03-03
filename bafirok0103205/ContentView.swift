16acdd79bcb711bc98d7dc005edc00c047ca6f07
aslanyasar <aslan.yasar@gmail.com>
2025-03-03 20:33:59 +0300

    Eski projeden dosyalar taşındı

---
+++ bafirok0103205/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bafirok0103205
+//
+//  Created by YAŞAR ASLAN on 1.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bafirok0103205/bafirok0103205App.swift
@@ -0,0 +1,17 @@
+//
+//  bafirok0103205App.swift
+//  bafirok0103205
+//
+//  Created by YAŞAR ASLAN on 1.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct bafirok0103205App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
